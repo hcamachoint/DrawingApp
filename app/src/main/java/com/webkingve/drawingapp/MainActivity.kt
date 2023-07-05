17d69fd40d0d12e2479ba6f5a5854169d1c6990a
dunicode <dunicode@MacBook-Pro-de-dunicode.local>
2023-07-04 20:22:06 -0400

    feat: setting images background

--- app/src/main/java/com/webkingve/drawingapp/MainActivity.kt
+++ app/src/main/java/com/webkingve/drawingapp/MainActivity.kt
@@ -13,46 +13,37 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import android.Manifest
+import android.content.Intent
+import android.provider.MediaStore
+import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
-
-    val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                // Permission is granted. Continue the action or workflow in your
-                // app.
-            } else {
-                // Explain to the user that the feature is unavailable because the
-                // feature requires a permission that the user has denied. At the
-                // same time, respect the user's decision. Don't link to system
-                // settings in an effort to convince the user to change their
-                // decision.
-            }
+    val openGalleryLauncher:ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
+        if (result.resultCode == RESULT_OK && result.data != null){
+            val imageBackground:ImageView = findViewById(R.id.iv_background)
+            imageBackground.setImageURI(result.data?.data)
         }
-
+    }
 
     val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
             permissions.entries.forEach {
                 val perMissionName = it.key
                 val isGranted = it.value
-                //Todo 3: if permission is granted show a toast and perform operation
                 if (isGranted ) {
                     Toast.makeText(
                         this@MainActivity,
                         "Permission granted now you can read the storage files.",
                         Toast.LENGTH_LONG
                     ).show()
-                    //perform operation
+
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
                 } else {
-                    //Todo 4: Displaying another toast if permission is not granted and this time focus on
-                    //    Read external storage
                     if (perMissionName == Manifest.permission.READ_EXTERNAL_STORAGE)
                         Toast.makeText(
                             this@MainActivity,
@@ -132,20 +123,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestStoragePermission(){
-        //Todo 6: Check if the permission was denied and show rationale
         if (
             ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
         ){
-            //Todo 9: call the rationale dialog to tell the user why they need to allow permission request
             showRationaleDialog("Kids Drawing App","Kids Drawing App " +
                     "needs to Access Your External Storage")
         }
         else {
-            // You can directly ask for the permission.
-            // Todo 7: if it has not been denied then request for permission
-            //  The registered ActivityResultCallback gets the result of this request.
             requestPermission.launch(
                 arrayOf(
                     Manifest.permission.READ_EXTERNAL_STORAGE,
